0aa2841278a14ace3e622b49aca01df1239e9a19
sebasavila2 <sebastianxrs4@gmail.com>
2024-09-07 06:18:24 +0000

    idk update what am i doing

--- Fracturing.c
+++ Fracturing.c
@@ -1,5 +1,19 @@
 #include <stdio.h>
 
-double calculate_distance();
+double calculateDistance()
 {
-       
+    double x1 = 0;
+    double y1 = 0;
+    double x2 = 0;
+    double y2 = 0;
+    printf("Point #1 entered: x1= ");
+    scanf("%f",&x1);
+    printf("; y1= ");
+    scanf("%f",&y1);
+    printf("Point #2 entered: x2= ");
+    scanf("%f",&x2);
+    printf("; y2= ");
+    scanf("%f",&y2);    
+    return 0;
+}    
+calculateDistance()
